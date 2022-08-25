2542612576ec864a54c123f62387d7a69a712847
William <williamhai@hotmail.com>
2022-08-25 20:58:05 +0200

    cleaned up esde derivation

--- pkgs/emulationstation-de/default.nix
+++ pkgs/emulationstation-de/default.nix
@@ -1,17 +1,13 @@
-{ lib, stdenv, fetchFromGitLab, cmake, SDL2, curl, ffmpeg,
-pugixml, pkgconf, freetype, freeimage, alsa-lib }:
+{ lib, stdenv, fetchFromGitLab, cmake, SDL2, curl, ffmpeg, pugixml, pkgconf, freetype, freeimage, alsa-lib }:
 
-let
+stdenv.mkDerivation rec{
+  name = "emulationstation-de";
   version = "1.2.6";
-
-in stdenv.mkDerivation {
-  pname = "emulationstation-de";
-  inherit version;
 
   src = fetchFromGitLab {
     owner  = "es-de";
     repo   = "emulationstation-de";
-    rev    = "v${version}";
+    rev    = "${version}";
     sha256 = "BiQnHtcKheEhwp0KKy9BCDIuZuAjmS8tWNyNw4nl5Fk=";
   };
 
@@ -19,22 +15,8 @@ in stdenv.mkDerivation {
 
   buildInputs = [ SDL2 curl cmake ffmpeg pugixml pkgconf freetype freeimage alsa-lib ];
 
-#   # https://gitlab.com/es-de/emulationstation-de/-/blob/master/INSTALL.md#building-on-unix
-#   configurePhase = ''
-#     cmake .
-#   '';
-#
-#   buildPhase = ''
-#     make -j$NIX_BUILD_CORES
-#   '';
-#
-#   installPhase = ''
-#     mkdir -p $out/bin
-#     mv * $out/bin
-#   '';
-
   meta = with lib; {
-    description = "EmulationStation Desktop Edition (ES-DE) is a frontend for browsing and launching games from your multi-platform game collection.";
+    description = "A frontend for browsing and launching games from your multi-platform game collection.";
     homepage    = "https://es-de.org/";
     license = licenses.mit;
     platforms   = lib.platforms.linux;
